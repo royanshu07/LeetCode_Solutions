a8769f6a120c5bc6fd8990a3afc5138da8a16ea3
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-05-01 19:42:27 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (63.35%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,19 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class ans{
+    ListNode head;
+    ListNode prev;
+}
+
 class Solution {
+    
     public ListNode reverseList(ListNode head) {
-        if(head==null||head.next==null){
-            return head;
+        ans re = rev2(head);
+        return re.head;
+        
+    }
+    
+    public ans rev2(ListNode head){
+        ans re = new ans();
+        if(head==null){
+            re.head= head;
+            re.prev = head;
+            return re;
         }
-        ListNode temp = reverseList(head.next);
-        ListNode temp2=head.next;
-        head.next= null;
-       
-        temp2.next=head;
-        //head.next = null;
-        
-        return temp;
-        
+        if(head.next==null){
+            re.head=head;
+            re.prev = head;
+            return re;
+        }
+         re = rev2(head.next);
+         head.next = null;
+         re.prev.next = head;
+         re.prev = head;
+         return re;
     }
 }
