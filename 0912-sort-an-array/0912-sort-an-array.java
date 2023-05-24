bafff3e052dd28278925f398252201ca0bd4ac8b
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-05-25 03:00:40 +0530

    Time: 43 ms (45.21%), Space: 55.9 MB (7.49%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,16 +1,51 @@
-import java.util.*;
 class Solution {
-    public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i =0;i<nums.length;i++){
-            pq.add(nums[i]);
-        }
-         for(int i =0;i<nums.length;i++){
-            nums[i]=pq.poll();
-        }
+    
         
-        return nums;
-        
-        
-    }
+    
+   public int[] sortArray(int[] nums) {
+		int len = nums.length;
+		for(int i = len/2;i>=0;i--){
+			downheapify(nums,i,len);
+		}
+
+		int size = len;
+	
+			while(size>1){
+			int temp = nums[size-1];
+			nums[size-1]= nums[0];
+			nums[0]=temp;
+			size--;
+			downheapify(nums,0,size);
+			
+		}
+       return nums;
+		
+	}
+
+	public static void downheapify(int[]arr,int start , int end){
+		int parent = start;
+		int leftchild = 2*parent+1;
+		int rightchild = leftchild+1;
+		int minindex = parent;
+		while(leftchild<end){
+            if(arr[leftchild]>arr[minindex]){
+				minindex = leftchild;
+			}
+			if(rightchild<end&&(arr[rightchild]>arr[minindex])){
+				minindex = rightchild;
+			}
+			if(minindex == parent){
+				return;
+			}
+			int temp = arr[minindex];
+			arr[minindex]= arr[parent];
+			arr[parent ]= temp;
+			parent = minindex;
+			leftchild= parent*2+1;
+			rightchild= leftchild+1;
+		}
+
+	}
+
 }
+ 
