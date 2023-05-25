64216a54fbb36c317dcc82b4704170bd3ba87d96
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-05-25 15:23:22 +0530

    Time: 104 ms (18.77%), Space: 43.8 MB (88.11%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,52 +8,65 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-//intializing and 
-// O(klogk)+n*O(logk)+n*O(logk) = O(nlogk)
 class Solution {
-    class helper implements Comparator<ListNode>{
-       
-        @Override
-            public int compare(ListNode o1,ListNode o2){
-                if (o1.val<o2.val)
-                    return -1;
-                else if (o1.val==o2.val)
-                    return 0;
-                else 
-                    return 1;
-            }
-}
-
-    
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists==null||lists.length==0) {return null;}
-        helper he = new helper();
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length,he);
-        for(int i = 0;i<lists.length;i++){
-            if(lists[i]!=null){
-            pq.add(lists[i]);}
+        if(lists==null||lists.length==0){
+            return null;
         }
-        ListNode orghead = pq.poll();
-        if(orghead==null){
-            return orghead;
-        }
-        if(orghead.next!=null){
-            pq.add(orghead.next);
-        }
-        ListNode head = orghead;
-        ListNode tail;
-        while(!pq.isEmpty()){
-            tail = pq.poll();
-            head.next = tail;
-            head = head.next;
-            if(tail.next!=null){
-                pq.add(tail.next);
-            }
-            
-        }
+        ListNode head = lists[0];
+        ListNode tail = lists[0];
+        ListNode pointer1;
+        ListNode pointer2;
         
-        return orghead;
-        
-        
+        for(int i = 1;i<lists.length;i++){
+           if(lists[i]==null){
+               continue;
+           }
+           else if(lists[0]==null){
+               lists[0]=lists[i];
+               head = lists[0];
+               tail = lists[0];
+               continue;
+           }
+           else{
+               pointer1 = lists[0];
+               pointer2 = lists[i];
+               if(pointer2.val<pointer1.val){
+                   head=pointer2;
+                   tail = pointer2;
+                   pointer2=pointer2.next;
+               }
+               else{
+                   head = lists[0];
+                   tail = head;
+                   pointer1=pointer1.next;
+               }
+               while(pointer1!=null&&pointer2!=null){
+                   if(pointer1.val<=pointer2.val){
+                       tail.next = pointer1;
+                       tail = tail.next;
+                       pointer1= pointer1.next;
+                   }
+                    else if(pointer2.val<pointer1.val){
+                       tail.next = pointer2;
+                       tail = tail.next;
+                       pointer2=pointer2.next;
+                   }
+                   
+               }
+               if(pointer1!=null){
+                   tail.next = pointer1;
+                   tail = tail.next;
+               }
+               if(pointer2!=null){
+                   tail.next=pointer2;
+                   tail = tail.next;
+               }
+               lists[0]=head;
+               
+           }
+        }
+        return head;
     }
+    
 }
