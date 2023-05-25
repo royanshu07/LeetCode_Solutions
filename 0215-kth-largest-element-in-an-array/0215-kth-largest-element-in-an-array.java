345a72c7924fd998d8404c1727eae0387729c5c3
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-05-25 20:05:13 +0530

    Time: 18 ms (87.85%), Space: 54.1 MB (25.78%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,16 +1,45 @@
 class Solution {
+    
+    public static void downheapify(int[] nums, int start , int end){
+       int parent = start;
+       int left = 2*start+1;
+       int right = 2*start +2;
+       int minindex = parent;
+       while(left<=end){
+           if(nums[minindex]>nums[left]){
+               minindex = left;
+           }
+            if(right<=end&&nums[minindex]>nums[right]){
+               minindex = right;
+           }
+           if(minindex==parent){
+               return;
+           }
+           else{
+                int temp = nums[minindex];
+                nums[minindex] = nums[parent];
+                nums[parent]= temp;
+                parent = minindex;
+                left = parent*2+1;
+                right = left+1;
+               
+           }
+       }
+    
+    }
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        for(int i = 0;i<k;i++){
-           pq.add(nums[i]);
-        }
-        for(int j = k;j<nums.length;j++){
-            if(pq.peek()<nums[j]){
-                pq.poll();
-                pq.add(nums[j]);
+       for(int i = k/2;i>=0;i--){
+           downheapify(nums,i,k-1);
+       }
+        for(int i =k;i<nums.length;i++){
+            if(nums[i]>nums[0]){
+                int temp = nums[i];
+                nums[i] = nums[0];
+                nums[0]= temp;
+                downheapify(nums,0,k-1);
             }
         }
-        return pq.poll();
-        
+        return nums[0];
+       
     }
 }
