eaa1f6ca2acb82e50252604e0c2836860655ef94
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-06-01 17:51:47 +0530

    Time: 2 ms (75.25%), Space: 41.1 MB (40.37%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,77 +1,28 @@
-class Element implements Comparable<Element>{
-    char a;
-    int freq;
-    
-    public Element(char a , int freq){
-        this.a = a;
-        this.freq = freq;
-    }
-     @Override
-    public int compareTo(Element e) {
-        return this.freq - e.freq;
-    }
-}
-
 class Solution {
     public String reorganizeString(String s) {
-        int[]arr = new  int[26];
-        int reqlen=0;
-        for(int i = 0;i<s.length();i++){
-            
-            arr[s.charAt(i)-'a']++;
-            if(arr[s.charAt(i)-'a']==1){
-                reqlen++;
-            }
-        }
+        int m[]=new int[26];//Hashmap to store the frequency of each character
+        int n=s.length();
+        for(int i=0;i<n;i++)
+            m[s.charAt(i)-'a']++;
         
-        Element[]pq = new Element[reqlen];
-        int size2=0;
-        for(int i = 0;i<arr.length;i++){
-            int freq = arr[i];
-            if(freq!=0){
-                Element e = new Element(((char)('a'+i)),freq);
-                pq[size2]=e;
-                size2++;
-            }
-        }
-        Arrays.sort(pq, Collections.reverseOrder());
-        char[]out = new char[s.length()];
-        int size = 0;
-        int pointer = 0;
-        int pos  =0;
-        Element e = pq[0];
-         int freq = e.freq;
-        while(size<s.length()){
-            if(freq<=0){
-                pos++;
-              e = pq[pos];
-            freq = e.freq;}
-            if(pointer>=s.length()){
-                pointer = 0;
-            }
-            while(out[pointer]!='\0'){
-                pointer++;
-            }
-            while(pointer<s.length()&&freq>0){
-                out[pointer]=e.a;
-                size++;
-                freq--;
-                if(pointer>0&&out[pointer]==out[pointer-1]){
-                    return "";
+        StringBuilder ans=new StringBuilder();
+        int i=0;
+        char prev='*';//Keep track of previous character
+        while(i<n){
+            int maxi=0,ind=0;
+            //We traverse the entire hashmap to find the character with maximum count and not equal to previous character
+            for(int j=0;j<26;j++){                
+                if(m[j]>maxi&&prev!=(j+'a')&&m[j]>0){
+                    maxi=m[j];
+                    ind=j;
                 }
-                pointer = pointer+2;
-                
             }
-        }
-        
-        String outs="";
-        for(char c:out){
-            outs+=c;
-            
-        }
-        
-        return outs;
-        
-       
+            if(maxi==0) return "";//If maxi is 0 then no solution can be made as suitable element not found
+            prev=(char)(ind+'a');//Updating previous
+            ans.append(prev);
+            m[ind]--;  //Decreasing count from map since the character is done
+            i++;
+        }        
+        return ans.toString();
     }
 }
