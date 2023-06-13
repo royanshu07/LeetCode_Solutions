c3911d2d499d21dfd271ab1bde0af6a804ab0c9a
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-06-13 22:41:17 +0530

    Time: 3 ms (38.23%), Space: 44.4 MB (5.73%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,38 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer>small = new ArrayList<Integer>();
-        List<List<Integer>> finalout = new ArrayList<>();
-        return combinationSum( candidates, target, 0,small,finalout);
+       List<Integer> small = new ArrayList<>();
+       List<List<Integer>> finalout = new ArrayList<>();
+       combinationSum(candidates,target,  small,finalout,0);
+       return finalout;
+        
+        
         
     }
-    public List<List<Integer>> combinationSum(int[] candidates, int target, int start,List<Integer>small,  List<List<Integer>> finalout ) {
-       
-        if(target==0){
-            finalout.add(small);
-            return finalout;
-            
-        }
-        if(start ==candidates.length||target<0){
-            return finalout;
-        }
+     public void combinationSum(int[] candidates,int target,List<Integer> small,List<List<Integer>> finalout,int start) {
+         if(target==0){
+             finalout.add(new ArrayList<>(small));
+             return;
+         }
+         if(target<0){
+             return;
+         }
+         for(int i = start;i<candidates.length;i++){
+             small.add(candidates[i]);
+             combinationSum(candidates,target-candidates[i],  small,finalout,i); 
+             small.remove(small.size()-1);
+             
+         }
+         return ;
          
-        
-          combinationSum( candidates, target, start+1, new ArrayList<>(small),finalout);
-          //small.add(candidates[start]);
-           //List<Integer> temp =  new ArrayList<>(small);
-          int k = target;
-           k = k-candidates[start];
-          while(k>=0){
-          
-           small.add(candidates[start]);
-           combinationSum( candidates, k, start+1, new ArrayList<>(small),finalout);
-           k = k-candidates[start];
-                                  }
-          
-          
-   
-        
-        return finalout;
-        
-        
-    }}
+         
+         
+         
+     }
+}
