d523bb530bf75e1d61ee000fd7508594311a5bd2
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-06-20 01:07:48 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -45,35 +45,55 @@ class gfg
 
 // } Driver Code Ends
 
-
-class Solution 
-{   
-    static int knapSack(int W, int wt[], int val[], int n){
-        int[][]dp = new int[n][W];
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<W;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return knapSack2( W, wt, val,n,dp);
+//Memoization
+// class Solution 
+// {   
+//     static int knapSack(int W, int wt[], int val[], int n){
+//        int[][]dp  = new int[n][W];
+//         for(int i = 0;i<n;i++){
+//             for(int j = 0;j<W;j++){
+//                 dp[i][j]=-1;
+//             }
+//         }
+//         return knapSack2( W, wt, val,n,dp);
         
-    }
-    //Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack2(int W, int wt[], int val[], int n,int[][]dp) 
-    { 
-         if(n==0||W==0){
-             return 0;
-         }
-         if(dp[n-1][W-1]!=-1){
-             return dp[n-1][W-1];
-         }
-         if(W<wt[n-1]){
-             return knapSack2(W,wt,val,n-1,dp);
-         }
-         int profit1 = val[n-1]+knapSack2(W-wt[n-1],wt,val,n-1,dp);
-         int profit2 = knapSack2(W,wt,val,n-1,dp);
-         dp[n-1][W-1]=Math.max(profit1,profit2);
-         return dp[n-1][W-1];
-}}
+//     }
+//     //Function to return max value that can be put in knapsack of capacity W.
+//     static int knapSack2(int W, int wt[], int val[], int n,int[][]dp) 
+//     { 
+//          if(n==0||W==0){
+//              return 0;
+//          }
+//          if(dp[n-1][W-1]!=-1){
+//              return dp[n-1][W-1];
+//          }
+//          if(W<wt[n-1]){
+//              return knapSack2(W,wt,val,n-1,dp);
+//          }
+//          int profit1 = val[n-1]+knapSack2(W-wt[n-1],wt,val,n-1,dp);
+//          int profit2 = knapSack2(W,wt,val,n-1,dp);
+//          dp[n-1][W-1]=Math.max(profit1,profit2);
+//          return dp[n-1][W-1];
+// }}
 
-
+//Tabulation 
+class Solution 
+ {   
+     static int knapSack(int W, int wt[], int val[], int n){
+          int[][]dp  = new int[n+1][W+1];
+          for(int i = 0;i<=n;i++){
+              dp[i][0]=0;
+          }
+          for(int i = 0;i<=W;i++){
+              dp[0][i]=0;
+          }
+          for(int i = 1;i<=n;i++){
+              for(int j = 1;j<=W;j++){
+                  if(j<wt[i-1]){
+                    dp[i][j]= dp[i-1][j];  
+                  }
+                  else{
+                  dp[i][j]= Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+              }
+          }}
+          return dp[n][W];}}
