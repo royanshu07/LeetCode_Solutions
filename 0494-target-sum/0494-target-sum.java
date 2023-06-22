962646619e5f4d7752b4fc88b18ea6a3609ee758
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-06-23 00:26:44 +0530

    Time: 4 ms (94.40%), Space: 43.4 MB (26.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,31 +1,37 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        HashMap<String,Integer> map = new HashMap<>();
-        return findTargetSumWays(nums,  target,0,0,map);
+        int sum = 0;
+        for(int i = 0;i<nums.length;i++){
+            sum+=nums[i];
+            
+       }
+       if(target>sum){
+           return 0;}
+        if((sum-target)%2!=0){
+            return 0;
+        }
+        int s2 = (sum-target)/2;
         
+        int[][]dp = new int[nums.length+1][s2+1];
         
-    }
-     public int findTargetSumWays(int[] nums, int target,int sum,int start,HashMap<String,Integer> map) {
+        dp[0][0]=1;
+        for(int j = 1;j<=s2;j++){
+            dp[0][j]=0;
+        }
+        
+        for(int i = 1;i<=nums.length;i++){
+            for(int j = 0;j<=s2;j++){
+                if(nums[i-1]>j){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];}
+            }
+        }
+        return dp[nums.length][s2];
            
-      
-          if(start==nums.length){
-             if(target==sum){
-              return 1;}
-              else{
-                  return 0;
-              }
-         }
-         String ke = start+","+sum;
-         if(map.containsKey(ke)){
-             return map.get(ke);
-         }
-         if(start>nums.length){
-             return 0;
-         }
-         int first = findTargetSumWays(nums,  target, sum+nums[start],start+1,map);
-         int second = findTargetSumWays(nums,  target, sum-nums[start],start+1,map);
-         map.put(ke,first+second);
-         return first+second;
+       
+        
         
     }
 }
