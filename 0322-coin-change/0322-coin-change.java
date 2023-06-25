4b4a58bc9ae80b295b6731d1e534d4a26a28d167
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-06-25 16:40:20 +0530

    Time: 38 ms (22.73%), Space: 44.5 MB (6.97%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -3,43 +3,40 @@ import java.util.Arrays;
 class Solution {
     public int coinChange(int[] coins, int amount) {
         Arrays.sort(coins);
-        int[][][] dp = new int[coins.length + 1][amount + 1][2];
+        int[][] dp = new int[coins.length + 1][amount + 1];
         for (int i = 0; i <= coins.length; i++) {
             for (int j = 0; j <= amount; j++) {
-                dp[i][j][0] = -1;
-            }
-        }
-        int res = coinChange(coins, amount, coins.length, 0, dp);
-        if (res == Integer.MAX_VALUE) {
+                dp[i][j]=-1;
+        }}
+        int res = coinChange(coins, amount, coins.length, dp);
+        if (res >= Integer.MAX_VALUE-1) {
             return -1;
         }
         return res;
     }
 
-    public int coinChange(int[] coins, int amount, int n, int no, int[][][] dp) {
+    public int coinChange(int[] coins, int amount, int n,int[][]dp) {
         if (amount == 0) {
-            if(dp[n][amount][0]!=-1&&dp[n][amount][0]<no){
-               return dp[n][amount][0]; 
-            }
-            dp[n][amount][0] = no;
-            return dp[n][amount][0];
+            
+            dp[n][amount] = 0;
+            return dp[n][amount];
         }
         if (n == 0) {
-            dp[n][amount][0] = Integer.MAX_VALUE;
-            return dp[n][amount][0];
+            dp[n][amount] = Integer.MAX_VALUE-1;
+            return dp[n][amount];
         }
-        if (dp[n][amount][0] != -1) {
-            if (dp[n][amount][1] <= no) {
-                return dp[n][amount][0];
-            }
+        if (dp[n][amount] != -1) {
+           
+                return dp[n][amount];
+            
         }
         if (amount < coins[n - 1]) {
-            return coinChange(coins, amount, n - 1, no, dp);
+            return coinChange(coins, amount, n - 1, dp);
         }
-        int a = coinChange(coins, amount - coins[n - 1], n, no + 1, dp);
-        int b = coinChange(coins, amount, n - 1, no, dp);
-        dp[n][amount][0] = Math.min(a, b);
-        dp[n][amount][1] = no;
-        return dp[n][amount][0];
+        int a = 1+coinChange(coins, amount - coins[n - 1], n, dp);
+        int b = coinChange(coins, amount, n - 1, dp);
+        dp[n][amount] = Math.min(a, b);
+      
+        return dp[n][amount];
     }
 }
