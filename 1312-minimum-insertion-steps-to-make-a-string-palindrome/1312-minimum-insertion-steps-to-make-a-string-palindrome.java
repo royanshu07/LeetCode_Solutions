e3514d1466ac411750571a9a0852e717576d1a8d
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-06-27 23:16:03 +0530

    Time: 52 ms (17.54%), Space: 44.6 MB (58.09%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minInsertions(String s) {
+         String  b  = "";
+        for(int i = s.length()-1;i>=0;i--){
+            b+=s.charAt(i);
+        }
+        int[][]dp = new int[s.length()+1][b.length()+1];
+         for(int i =0;i<=s.length();i++){
+             dp[i][0]=0;}
+        for(int i =0;i<=s.length();i++){
+             dp[0][i]=0;}
+        for(int i =1;i<=s.length();i++){
+            for(int j = 1;j<=b.length();j++){
+                if(s.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                   int x =  dp[i-1][j];
+                   int y =  dp[i][j-1];
+                   dp[i][j]=Math.max(x,y);
+                } 
+                
+            }
+        }
+        return s.length()-dp[b.length()][b.length()];
+        
+    }
+}
