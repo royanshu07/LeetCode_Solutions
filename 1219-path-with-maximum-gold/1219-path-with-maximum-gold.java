4371cc474caeab869edc7fcd7bc822b4105c77d5
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-06-29 15:58:08 +0530

    Time: 76 ms (71.98%), Space: 40.2 MB (60.65%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -2,18 +2,14 @@ class Solution {
     public int getMaximumGold(int[][] grid) {
         int m = grid.length;
         int n  = grid[0].length;
-        int[][]dp= new int[m][n];
+        
         int max = 0;
-        for(int i = 0;i<dp.length;i++){
-            for(int j = 0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-               
-            }
-        }
-        for(int i = 0;i<dp.length;i++){
-            for(int j = 0;j<dp[0].length;j++){
+        
+       
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
                 if(grid[i][j]!=0){
-            int ans = helper(i,j,grid,dp,0);
+            int ans = helper(i,j,grid,0);
                     if(ans>max){
                         max= ans;
                     }
@@ -23,30 +19,26 @@ class Solution {
         }
         return max;
        }
-    public int helper(int i , int j , int[][] grid,int[][]dp,int curr) {
+    public int helper(int i , int j , int[][] grid,int curr) {
        
         if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]==0){
             return curr;
         }
         
-        if(dp[i][j]==Integer.MAX_VALUE){
+        if(grid[i][j]==Integer.MAX_VALUE){
             return 0;
         }
-        //  if(dp[i][j]!=-1){
-        //     return dp[i][j];
-        // }
-        // if(grid[i][j]==0){
-        //     return curr;
-        // }
-        dp[i][j]=Integer.MAX_VALUE;
+        int temp = grid[i][j];
         curr+=grid[i][j];
-        int left = helper(i,j-1,grid,dp,curr);
-        int right = helper(i,j+1,grid,dp,curr);
-        int up = helper(i-1,j,grid,dp,curr);
-        int down = helper(i+1,j,grid,dp,curr);
+        grid[i][j]=Integer.MAX_VALUE;
         
-        dp[i][j]=Math.max(Math.max(left,right),Math.max(up,down));
+        int left = helper(i,j-1,grid,curr);
+        int right = helper(i,j+1,grid,curr);
+        int up = helper(i-1,j,grid,curr);
+        int down = helper(i+1,j,grid,curr);
+        grid[i][j] = temp;
+        return Math.max(Math.max(left,right),Math.max(up,down));
         //System.out.println(dp[i][j]);
-        return dp[i][j];
+        
         
 }}
