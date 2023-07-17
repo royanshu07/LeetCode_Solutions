600bf18674408e18489d650d02680de0958a2d97
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-07-17 15:46:36 +0530

    Time: 5 ms (99.78%), Space: 43.8 MB (79.88%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,78 @@
+// class Solution {
+//     public List<Integer> findAnagrams(String s, String p) {
+//         int[]temp = new int[26];
+//         int[]map = new int[26];
+//         for(int i = 0 ;i<p.length();i++){
+//             temp[p.charAt(i)-'a']++;
+//             //System.out.println(p.charAt(i)-'a'+" "+  temp[p.charAt(i)-'a']);
+//         }
+//         List<Integer> ans = new ArrayList<>();
+//         int i = 0;
+//         int j = 0;
+//         int k = p.length();
+//         while(j<s.length()){
+//             map[s.charAt(j)-'a']++;
+//             //System.out.println(s.charAt(j)-'a'+" "+ map[s.charAt(j)-'a']);
+//             if(i-j+1<k){
+//                 j++;
+//             }
+//             else{
+//                boolean isequal = true;
+//                for(int x = 0;x<p.length();x++){
+//                    char c = p.charAt(x);
+//                    if(temp[c-'a']!=map[c-'a']){
+//                        isequal = false;
+//                        break;
+//                    }
+                   
+//                }
+//                if(isequal){
+//                    ans.add(i);
+//                }
+                   
+//                 map[s.charAt(i)-'a']--;
+//                 i++;
+//                 j++;
+//             }
+//         }
+//         return ans;
+        
+//     }
+// }
+
+
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] temp = new int[26];
+        int[] map = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            temp[p.charAt(i) - 'a']++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        int k = p.length();
+        while (j < s.length()) {
+            map[s.charAt(j) - 'a']++;
+            if (j - i + 1 < k) {
+                j++;
+            } else {
+                boolean isAnagram = true;
+                for (int x = 0; x < 26; x++) {
+                    if (temp[x] != map[x]) {
+                        isAnagram = false;
+                        break;
+                    }
+                }
+                if (isAnagram) {
+                    ans.add(i);
+                }
+                map[s.charAt(i) - 'a']--;
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+}
