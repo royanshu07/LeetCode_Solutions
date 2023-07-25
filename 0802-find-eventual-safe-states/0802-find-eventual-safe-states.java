2bec1d5e6cf28d76eacff5391b044a08d2e12e12
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-07-25 18:23:04 +0530

    Time: 14 ms (57.81%), Space: 54.9 MB (72.18%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -2,14 +2,14 @@ class Solution {
     List<Integer> answer;
     public List<Integer> eventualSafeNodes(int[][] graph) {
         answer = new ArrayList<>();
-        int[]path = new int[graph.length];
+        //int[]path = new int[graph.length];
         int[]visit = new int[graph.length];
         
         for(int i =0 ;i<graph.length;i++){
             if(visit[i]==0){
-                visit[i]=1;
-                path[i]=1;
-                dfs(i,graph,answer,visit,path);
+                visit[i]=2;
+                //path[i]=1;
+                dfs(i,graph,answer,visit);
             }
         }
         Collections.sort(answer);
@@ -18,28 +18,30 @@ class Solution {
     }
     
     
-    public boolean dfs(int curr,int[][]graph, List<Integer> answer,int[]visit,int[]path){
+    public boolean dfs(int curr,int[][]graph, List<Integer> answer,int[]visit){
         int[]adj = graph[curr];
         if(adj==null||adj.length==0){
-            path[curr]=0;
+            visit[curr]=1;
+            //path[curr]=0;
             answer.add(curr);
             return true;
         }
         
         for(int i:adj){
-            if(path[i]==1){
+            if(visit[i]==2){
                return false; 
             }
             if(visit[i]==1){
                 continue;
             }
-            path[i]=1;
-            visit[i]=1;
-           if (dfs(i,graph,answer,visit,path)==false){
+            //path[i]=1;
+            visit[i]=2;
+           if (dfs(i,graph,answer,visit)==false){
               return false; 
            }
         }
-        path[curr]=0;
+        visit[curr]=1;
+       // path[curr]=0;
         answer.add(curr);
         return true;
         
