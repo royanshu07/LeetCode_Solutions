dd0b585485e22ac7cad853d5940163dbaa5362cf
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-07-27 00:57:07 +0530

    Time: 4 ms (5.92%), Space: 40.6 MB (68.42%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<>();
-        List<Character> left = new ArrayList<>();
+       
         
         HashMap<Character,Character> map = new HashMap<>();
         map.put(')','(');
         map.put('}','{');
         map.put(']','[');
-        left.add('(');
-        left.add('{');
-        left.add('[');
+        
         int count = 0;
         for(int i = 0;i<s.length();i++){
-            if(left.contains(s.charAt(i))){
+           if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 stk.add(s.charAt(i));
+                continue;
             }
             else if(!stk.isEmpty()&&map.get(s.charAt(i))==stk.peek()){
                 stk.pop();
             }
             else{
-                count++;
+                return false;
             }
             
         }
