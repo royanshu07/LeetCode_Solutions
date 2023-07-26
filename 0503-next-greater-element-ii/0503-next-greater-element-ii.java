3f060bae03703cdac54f85fb0844dc6cf312d4c5
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-07-27 02:45:38 +0530

    Time: 40 ms (32.53%), Space: 45.6 MB (12.83%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -7,7 +7,7 @@ class Solution {
         }
         
         
-        int[]helper = new int[nums2.length];
+        int[]helper = new int[nums.length];
          Stack<Integer> stk = new Stack<>();
         
          for (int i = nums2.length - 1; i >= 0; i--) {
@@ -15,19 +15,17 @@ class Solution {
                 stk.pop();
             }
             if (!stk.isEmpty()) {
-                helper[i] = stk.peek();
+                if(i<nums.length){
+                helper[i] = stk.peek();}
             } else {
-                helper[i] = -1;
+                if(i<nums.length){
+                helper[i] = -1;}
             }
             stk.push(nums2[i]);
         }
         
-        int[]answer = new int[nums.length];
-        for(int i = 0;i<answer.length;i++){
-            answer[i]=helper[i];
-        }
         
-       return answer;
+       return helper;
     }
         
         
