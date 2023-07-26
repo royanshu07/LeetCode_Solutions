2f11bd7726b6238a04654eeb4bf1372cb318b0a5
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-07-27 02:10:28 +0530

    Time: 4 ms (80.65%), Space: 43.7 MB (53.10%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,35 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> stk = new Stack<>();
-        int[]helper = new int[nums2.length];
+        HashMap<Integer,Integer> map = new HashMap<>();
          for (int i = nums2.length - 1; i >= 0; i--) {
             while (!stk.isEmpty() && stk.peek() <= nums2[i]) {
                 stk.pop();
             }
             if (!stk.isEmpty()) {
-                helper[i] = stk.peek();
+                map.put(nums2[i],stk.peek());
             } else {
-                helper[i] = -1;
+                
             }
             stk.push(nums2[i]);
         }
         
         
         for(int i = 0;i<nums1.length;i++){
-            int a = findindex(nums1[i],nums2);
-            nums1[i]=helper[a];
+           
+            nums1[i]=map.getOrDefault(nums1[i],-1);
         }
         
         return nums1;
     }
     
-    public static int findindex(int a ,int[]arr ){
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i]==a){
-              return i;  
-            }
-        }
-        //System.out.print("hiii");
-        return -1;
-    }
+    
 }
