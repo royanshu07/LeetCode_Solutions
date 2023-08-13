e0bd0eab815ff25f38c574aa11c7cf17211b15d5
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-08-13 11:39:11 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (71.64%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,23 +14,26 @@
  * }
  */
 class Solution {
+    static int count = 0;
     public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer>trav = new ArrayList<Integer>();
-        return kthSmallest(root,k,trav);
+        //ArrayList<Integer>trav = new ArrayList<Integer>();
+        count=0;
+        return kthSmallest2(root,k);
     }
-     public int kthSmallest(TreeNode root, int k,ArrayList<Integer>trav) {
+     public int kthSmallest2(TreeNode root, int k) {
          if(root==null){
              return Integer.MAX_VALUE;
          }
-         int a = kthSmallest( root.left, k,trav);
+         int a = kthSmallest2( root.left, k);
          if(a!=Integer.MAX_VALUE){
              return a;
          }
-         trav.add(root.val);
-         if(trav.size()==k){
+         count++;
+         if(count==k){
              return root.val;
          }
-         a = kthSmallest( root.right, k,trav);
+         a = kthSmallest2( root.right, k);
+         //System.out.println(count+" "+root.val);
          return a;
       
     }
