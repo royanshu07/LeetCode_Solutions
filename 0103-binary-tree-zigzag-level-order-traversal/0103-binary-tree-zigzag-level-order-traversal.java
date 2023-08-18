216c5e2198b2408e802073f56ca3ffad7e6d6a75
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-08-18 16:56:59 +0530

    Time: 1 ms (90.10%), Space: 41.5 MB (46.16%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,43 +15,41 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-       List<List<Integer>> out = levelOrder(root);
-        for(int i = 0;i<out.size();i++){
-            if(i%2!=0){
-                List<Integer> temp = out.get(i);
-                Collections.reverse(temp);
-                //out.set(i,temp);
+      
+        List<List<Integer>> out = new ArrayList<>();
+        if(root==null){
+            return out; 
+        }        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int flag = 0;
+        while(!q.isEmpty()){
+            List<Integer> temp =new ArrayList<>();
+            int size = q.size();
+            for(int i = 0;i<size;i++){
+                temp.add(0);
             }
+            
+               for(int i = 0;i<size;i++){
+                   TreeNode node = q.poll();
+                   if(flag==1){
+                       temp.set(size-i-1,node.val);
+                   }
+                   else{
+                   temp.set(i,node.val);}
+                   if(node.left!=null){
+                       q.add(node.left);
+                   }
+                   if(node.right!=null){
+                       q.add(node.right);
+                   }
+                   
+               }
+            flag = 1-flag;
+            out.add(temp);
+            
+            
         }
-        return out;
-    }
-      public List<List<Integer>> levelOrder(TreeNode root) {
-         List<List<Integer>> out = new ArrayList<>();
-         Queue<TreeNode> q = new LinkedList<>();
-         if(root==null){
-             return out;
-         }
-         q.add(root);
-         q.add(null);
-         List<Integer> temp = new ArrayList<>();
-         while(!q.isEmpty()){
-             TreeNode tempnode = q.poll();
-             if(tempnode==null){
-                 out.add(temp);
-                 temp = new ArrayList<>();
-                 if(q.size()!=0){
-                     q.add(null);
-                 }
-                 continue;
-             }
-             temp.add(tempnode.val);
-             if(tempnode.left!=null){
-                 q.add(tempnode.left);
-             }
-             if(tempnode.right!=null){
-                 q.add(tempnode.right);
-             }
-         }
         return out;
         
     }
