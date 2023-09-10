b347b8ffa0d43add5348ef11dc038828f76ed116
Anshu Roy <102736425+royanshu07@users.noreply.github.com>
2023-09-10 20:57:31 +0530

    Time: 49 ms (71.63%), Space: 116.4 MB (85.16%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,188 +1,57 @@
-class LRUCache {
-    class DoublyLnk{
-    int val;
-    int key;
-    DoublyLnk prev, next;
-    DoublyLnk(int val, int key, DoublyLnk p, DoublyLnk n){
-        this.val=val;
-        this.key=key;
-        this.prev=p;
-        this.next=n;
-    }
-    }
-    
-    int cty;
-    HashMap<Integer, DoublyLnk> see;
-    DoublyLnk head=null;
-    DoublyLnk ptr=null;
-    public LRUCache(int capacity) {
-        this.cty=capacity;
-        see=new HashMap<>();
-    }
-    
-    public void update(DoublyLnk node){
-        if(node==head&&node==ptr){
-            return;
-        }
-        else if(node==ptr){
-            return;
-        }
-        else if(node==head){
-            head=head.next;
-            head.prev=null;
-            ptr.next=node;
-            node.next=null;
-            node.prev=ptr;
-            ptr=ptr.next;
-        }
-        else{
-            DoublyLnk p=node.prev;
-            DoublyLnk n=node.next;
-            p.next=n;
-            n.prev=p;
-            ptr.next=node;
-            node.prev=ptr;
-            node.next=null;
-            ptr=ptr.next;
-        }
-    }
 
-    public void add(int key, int value){
-        if(head==null){
-            DoublyLnk x=new DoublyLnk(value, key, null, null);
-            head=x;
-            ptr=x;
-            see.put(key, x);
-        }
-        else{
-        DoublyLnk x=new DoublyLnk(value, key, ptr, null);
-        ptr.next=x;
-        ptr=x;
-        see.put(key, x);
-        }
+
+class LRUCache {
+    Node head = new Node(0, 0), tail = new Node(0, 0);
+    Map < Integer, Node > map = new HashMap();
+    int capacity;
+
+    public LRUCache(int _capacity) {
+        capacity = _capacity;
+        head.next = tail;
+        tail.prev = head;
     }
 
     public int get(int key) {
-        if(this.see.containsKey(key)){
-            update(see.get(key));
-            return this.see.get(key).val;
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        } else {
+            return -1;
         }
-        else return -1;
     }
-    
+
     public void put(int key, int value) {
-        if(see.containsKey(key)){
-            DoublyLnk temp=see.get(key);
-            update(temp);
-            temp.val=value;
+        if (map.containsKey(key)) {
+            remove(map.get(key));
         }
-        else if(see.size()+1>cty){
-        see.remove(head.key);
-        if(head.next==null){
-            head=null;
-            ptr=null;
+        if (map.size() == capacity) {
+            remove(tail.prev);
         }
-        else{
-        head=head.next;
-        head.prev=null;
-        }
-        add(key, value);
-        }
-        else{
-            add(key, value);
+        insert(new Node(key, value));
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+    class Node {
+        Node prev, next;
+        int key, value;
+        Node(int _key, int _value) {
+            key = _key;
+            value = _value;
         }
     }
 }
-
-// class LinkedList1{
-//     int key;
-//     int val;
-//     LinkedList1 left;
-//     LinkedList1 right;
-    
-    
-// } 
-// class LRUCache {
-//       HashMap<Integer,LinkedList1> map = new HashMap<>();
-//       int size;
-//       int currsize;
-//       LinkedList1 head;
-//       LinkedList1 tail;
-    
-//     public LRUCache(int capacity) {
-//         size = capacity;
-//         currsize = 0;
-//         head = new LinkedList1();
-//         tail = new LinkedList1();
-//         head.right = tail;
-//         tail.left = head;
-//         head.key = -1; // Add this line
-//        tail.key = -1;
-        
-        
-        
-//     }
-    
-//     public int get(int key) {
-//         if(!map.containsKey(key)){
-//             return -1;
-//         }
-//         LinkedList1 temp = map.get(key);
-//         int val = temp.val;
-        
-//         temp.left.right = temp.right;
-//         temp.right.left = temp.left;
-        
-//         LinkedList1 headleft = head.right;
-//         head.right = temp;
-//         headleft.left =temp;
-//         temp.left = head;
-//         temp.right= headleft;
-//         return val;
-        
-        
-//     }
-    
-//     public void put(int key, int value) {
-//        if(map.containsKey(key)){
-//            LinkedList1 temp = map.get(key);
-//            temp.val = value;
-//         temp.left.right = temp.right;
-//         temp.right.left = temp.left;
-        
-//         LinkedList1 headleft = head.right;
-//         head.left = temp;
-//         headleft.right =temp;
-//         temp.left = head;
-//         temp.right= headleft;
-//         return;
-//        } 
-//         if(currsize>=size){
-//             currsize--;
-//             LinkedList1 last = tail.left;
-//             tail.left = last.left;
-//             last.left.right = tail;
-//             map.remove(last.key);
-//         }
-//         currsize++;
-//         LinkedList1 temp = new LinkedList1();
-//         temp.val = value;
-//         temp.key = key;
-//         map.put(key,temp);
-//          LinkedList1 headleft = head.right;
-//         head.right = temp;
-//         headleft.left =temp;
-//         temp.left = head;
-//         temp.right= headleft;
-//         return;
-        
-        
-//     }
-// }
-
-// /**
-//  * Your LRUCache object will be instantiated and called as such:
-//  * LRUCache obj = new LRUCache(capacity);
-//  * int param_1 = obj.get(key);
-//  * obj.put(key,value);
-//  */
